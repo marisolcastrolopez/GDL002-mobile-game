250c89636eb904dbbe60ab0316cde188e7c263d1
marisolcastrolopez <solcastrodg@gmail.com>
2019-06-26 17:08:48 -0500

    Modal now shows up when user clicks on an icon

--- game/components/FiguresCountdown.js
+++ game/components/FiguresCountdown.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
-import { StyleSheet, View, ScrollView, Alert } from "react-native";
+import { StyleSheet, View, ScrollView, Alert, Button, Text } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 import CountdownGame from "./CountdownGame";
 import ModalExample from "./ModalExample";
+import Modal from "react-native-modal";
 
 export default class FiguresCountdown extends Component {
+  
   constructor(props) {
     super(props);
 
     this.state = {
-      points: 0
+      points: 0,
+      isModalVisible: false
     };
   }
   handleAddPoints = () => {
@@ -17,194 +20,188 @@ export default class FiguresCountdown extends Component {
       count: this.state.points + 1
     });
   };
+  toggleModal = () => {
+    this.setState({ isModalVisible: !this.state.isModalVisible });
+  };
 
   handleWrongAnswer = () => {
-    this.setState({
-      count: this.state.points + 1
-    });
+    navigate("FirstStartGame", { screen: FirstStartGame })
   };
 
   render() {
     const points = this.state.points;
     return (
       <>
-      <View style={[styles.row, styles.scrollViewMargin]}>
-        <View style={[styles.box]}>
-          <Icon.Button
-            name="circle"
-            size={30}
-            color="#11A0FF"
-            backgroundColor="transparent"
-            underlayColor="transparent"
-            onPress={() => Alert.alert(
-  'Alert Title',
-  'My Alert Msg',
-  [
-    {text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
-    {
-      text: 'Cancel',
-      onPress: () => console.log('Cancel Pressed'),
-      style: 'cancel',
-    },
-    {text: 'OK', onPress: () => console.log('OK Pressed')},
-  ],
-  {cancelable: false},
-)}
-          />
+        <View style={[styles.row, styles.scrollViewMargin]}>
+          <View style={[styles.box]}>
+            <Icon.Button
+              name="circle"
+              size={30}
+              color="#11A0FF"
+              backgroundColor="transparent"
+              underlayColor="transparent"
+              onPress={this.toggleModal}
+            />
+          </View>
+          <View style={[styles.box]}>
+            <Icon.Button
+              name="cloud"
+              size={30}
+              color="#7D26FF"
+              backgroundColor="transparent"
+              onPress={() => alert("Hi")}
+            />
+          </View>
+          <View style={[styles.box]}>
+            <Icon.Button
+              name="star"
+              size={30}
+              color="#304BFC"
+              backgroundColor="transparent"
+              onPress={() => alert("Hi")}
+            />
+          </View>
+          <View style={[styles.box]}>
+            <Icon.Button
+              name="certificate"
+              size={30}
+              color="#2AC427"
+              backgroundColor="transparent"
+              onPress={() => alert("Hi")}
+            />
+          </View>
         </View>
-        <View style={[styles.box]}>
-        <Icon.Button
-            name="cloud"
-            size={30}
-            color="#7D26FF"
-            backgroundColor="transparent"
-            onPress={() => alert("Hi")}
-          />
-        </View>
-        <View style={[styles.box]}>
-          <Icon.Button
-            name="star"
-            size={30}
-            color="#304BFC"
-            backgroundColor="transparent"
-            onPress={() => alert("Hi")}
-          />
-        </View>
-        <View style={[styles.box]}>
-          <Icon.Button
-            name="certificate"
-            size={30}
-            color="#2AC427"
-            backgroundColor="transparent"
-            onPress={() => alert("Hi")}
-          />
-        </View>
-      </View>
 
-      <View style={styles.row}>
-        <View style={[styles.box]}>
-          <Icon.Button
-            name="cloud"
-            size={30}
-            color="#FF50CE"
-            backgroundColor="transparent"
-            onPress={() => alert("Hi")}
-          />
+        <View style={styles.row}>
+          <View style={[styles.box]}>
+            <Icon.Button
+              name="cloud"
+              size={30}
+              color="#FF50CE"
+              backgroundColor="transparent"
+              onPress={() => alert("Hi")}
+            />
+          </View>
+          <View style={[styles.box]}>
+            <Icon.Button
+              name="heart"
+              size={30}
+              color="#11A0FF"
+              backgroundColor="transparent"
+              onPress={() => alert("Hi")}
+            />
+          </View>
+          <View style={[styles.box]}>
+            <Icon.Button
+              name="lock"
+              size={30}
+              color="#FF50CE"
+              backgroundColor="transparent"
+              onPress={() => alert("Hi")}
+            />
+          </View>
+          <View style={[styles.box]}>
+            <Icon.Button
+              name="leaf"
+              size={30}
+              color="#304BFC"
+              backgroundColor="transparent"
+              onPress={() => alert("Hi")}
+            />
+          </View>
         </View>
-        <View style={[styles.box]}>
-          <Icon.Button
-            name="heart"
-            size={30}
-            color="#11A0FF"
-            backgroundColor="transparent"
-            onPress={() => alert("Hi")}
-          />
-        </View>
-        <View style={[styles.box]}>
-          <Icon.Button
-            name="lock"
-            size={30}
-            color="#FF50CE"
-            backgroundColor="transparent"
-            onPress={() => alert("Hi")}
-          />
-        </View>
-        <View style={[styles.box]}>
-          <Icon.Button
-            name="leaf"
-            size={30}
-            color="#304BFC"
-            backgroundColor="transparent"
-            onPress={() => alert("Hi")}
-          />
-        </View>
-      </View>
 
-      <View style={styles.row}>
-        <View style={[styles.box]}>
-          <Icon.Button
-            name="paw"
-            size={30}
-            color="#FFD323"
-            backgroundColor="transparent"
-            onPress={() => alert("Hi")}
-          />
+        <View style={styles.row}>
+          <View style={[styles.box]}>
+            <Icon.Button
+              name="paw"
+              size={30}
+              color="#FFD323"
+              backgroundColor="transparent"
+              onPress={() => alert("Hi")}
+            />
+          </View>
+          <View style={[styles.box]}>
+            <Icon.Button
+              name="paw"
+              size={30}
+              color="#2AC427"
+              backgroundColor="transparent"
+              onPress={() => alert("Hi")}
+            />
+          </View>
+          <View style={[styles.box]}>
+            <Icon.Button
+              name="circle"
+              size={30}
+              color="#304BFC"
+              backgroundColor="transparent"
+              onPress={() => alert("Hi")}
+            />
+          </View>
+          <View style={[styles.box]}>
+            <Icon.Button
+              name="user"
+              size={30}
+              color="#FFD323"
+              backgroundColor="transparent"
+              onPress={() => alert("Hi")}
+            />
+          </View>
         </View>
-        <View style={[styles.box]}>
-          <Icon.Button
-            name="paw"
-            size={30}
-            color="#2AC427"
-            backgroundColor="transparent"
-            onPress={() => alert("Hi")}
-          />
-        </View>
-        <View style={[styles.box]}>
-          <Icon.Button
-            name="circle"
-            size={30}
-            color="#304BFC"
-            backgroundColor="transparent"
-            onPress={() => alert("Hi")}
-          />
-        </View>
-        <View style={[styles.box]}>
-          <Icon.Button
-            name="user"
-            size={30}
-            color="#FFD323"
-            backgroundColor="transparent"
-            onPress={() => alert("Hi")}
-          />
-        </View>
-      </View>
 
-      <View style={styles.row}>
-        <View style={[styles.box]}>
-          <Icon.Button
-            name="certificate"
-            size={30}
-            color="#FF50CE"
-            backgroundColor="transparent"
-            onPress={() => alert("Hi")}
-          />
+        <View style={styles.row}>
+          <View style={[styles.box]}>
+            <Icon.Button
+              name="certificate"
+              size={30}
+              color="#FF50CE"
+              backgroundColor="transparent"
+              onPress={() => alert("Hi")}
+            />
+          </View>
+          <View style={[styles.box]}>
+            <Icon.Button
+              name="star"
+              size={30}
+              color="#FF9837"
+              backgroundColor="transparent"
+              onPress={() => alert("Hi")}
+            />
+          </View>
+          <View style={[styles.box]}>
+            <Icon.Button
+              name="heart"
+              size={30}
+              color="#FF50CE"
+              backgroundColor="transparent"
+              onPress={() => alert("Hi")}
+            />
+          </View>
+          <View style={[styles.box]}>
+            <Icon.Button
+              name="heart"
+              size={30}
+              color="#11A0FF"
+              backgroundColor="transparent"
+              onPress={() => alert("Hi")}
+            />
+          </View>
         </View>
-        <View style={[styles.box]}>
-          <Icon.Button
-            name="star"
-            size={30}
-            color="#FF9837"
-            backgroundColor="transparent"
-            onPress={() => alert("Hi")}
-          />
-        </View>
-        <View style={[styles.box]}>
-        <Icon.Button
-            name="heart"
-            size={30}
-            color="#FF50CE"
-            backgroundColor="transparent"
-            onPress={() => alert("Hi")}
-          />
-        </View>
-        <View style={[styles.box]}>
-          <Icon.Button
-            name="heart"
-            size={30}
-            color="#11A0FF"
-            backgroundColor="transparent"
-            onPress={() => alert("Hi")}
-          />
-        </View>
-      </View>
-      <View style={styles.timerViewMargin}>
-        {/* <CountdownGame/> */}
-
-      </View>
-      {/* <View>
+        <View style={styles.timerViewMargin}>{/* <CountdownGame/> */}</View>
+        {/* <View>
       <ModalExample/>
       </View> */}
-    </>
+      <View style={{ flex: 1 }}>
+        <Button title="Show modal" onPress={this.toggleModal} />
+        <Modal isVisible={this.state.isModalVisible}>
+          <View style={{ flex: 1 }}>
+            <Text>Hello!</Text>
+            <Button title="Hide modal" onPress={this.toggleModal} />
+          </View>
+        </Modal>
+      </View>
+      </>
     );
   }
 }
